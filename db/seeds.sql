7237f6b1d1e2f55cd6caffe6190ff329b624b018
harithimad <harithimad@gmail.com>
2017-07-30 11:47:09 -0500

    adding files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Three entries to populate the new table created using schema.sql
+INSERT INTO burgers (burger_name,devoured) VALUES ('Cheeseburger with Onion',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Turkey Burger with Pepperjack',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Falafel Burger',false);
